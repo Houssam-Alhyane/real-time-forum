68e624d1d0a3fe2082820b761fb6e82e4ba0fba7
halhyane <houssamalhyan5@gmail.com>
2026-06-20 15:13:53 +0100

    update main

--- main.go
+++ main.go
@@ -11,23 +11,19 @@ import (
 func main() {
 
 	// Init DB
-	
-		if err := database.Init(); err != nil {
+	if err := database.Init(); err != nil {
 		log.Fatalf("Database initialization failed: %v", err)
 	}
-
 
 	// Serve static files (CSS + JS)
 	fs := http.FileServer(http.Dir("./frontend/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// Routes
-	
-
+	http.HandleFunc("/", handlers.Forum)
 	http.HandleFunc("/register", handlers.Register)
-/* 	http.HandleFunc("/login", handlers.Login)
+	http.HandleFunc("/login", handlers.Login)
 	http.HandleFunc("/logout", handlers.Logout)
-	http.HandleFunc("/", handlers.TemplateHandler)  */
 
 	// (later)
 	// http.HandleFunc("/posts", handlers.Posts)
